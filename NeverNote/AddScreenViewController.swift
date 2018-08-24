0f3b25c3caf665bab5ce8743be74f2eb442d7280
Aditya <aditya@INFLAP027.local>
2018-08-24 15:01:45 +0530

    Add Storyboard and ViewController

---
+++ NeverNote/AddScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddScreenViewController.swift
+//  NeverNote
+//
+//  Created by Aditya on 8/24/18.
+//  Copyright © 2018 Chandana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddScreenViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBAction func doneBarButton(_ sender: Any) {
+    }
+}
