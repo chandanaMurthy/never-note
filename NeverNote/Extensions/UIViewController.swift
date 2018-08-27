e2f37df90631419840915d4cb980f119e46e4dcb
Aditya <aditya@INFLAP027.local>
2018-08-27 12:35:58 +0530

    Revert back to original

--- NeverNote/Extensions/UIViewController.swift
+++ NeverNote/Extensions/UIViewController.swift
@@ -21,8 +21,6 @@ extension UIViewController {
         }
     }
     
-    
-    
     static let ADD_SCREEN_VIEW_CONTROLLER = "AddScreenViewController"
     static var addScreen : AddScreenViewController {
         get {
@@ -37,7 +35,7 @@ extension UIViewController {
     static let NOTES_VIEW_CONTROLLER = "NotesViewController"
     static var notes : NotesViewController {
         get {
-            let notesStoryboard = UIStoryboard.home
+            let notesStoryboard = UIStoryboard.notes
             guard let notesViewController = notesStoryboard.instantiateViewController(withIdentifier: NotesViewController.NOTES_VIEW_CONTROLLER) as? NotesViewController else {
                 fatalError("There is no identifier set as \(NotesViewController.NOTES_VIEW_CONTROLLER)")
             }
@@ -48,33 +46,11 @@ extension UIViewController {
     static let COMPLETED_TASKS_VIEW_CONTROLLER = "CompletedTasksViewController"
     static var completed : CompletedTasksViewController {
         get {
-            let completedTasksStoryboard = UIStoryboard.home
+            let completedTasksStoryboard = UIStoryboard.completed
             guard let completedTasksViewController = completedTasksStoryboard.instantiateViewController(withIdentifier: CompletedTasksViewController.COMPLETED_TASKS_VIEW_CONTROLLER) as? CompletedTasksViewController else {
                 fatalError("There is no identifier set as \(CompletedTasksViewController.COMPLETED_TASKS_VIEW_CONTROLLER)")
             }
             return completedTasksViewController
-        }
-    }
-    
-    static let TRASH_VIEW_CONTROLLER = "TrashViewController"
-    static var trash: TrashViewController {
-        get {
-            let trashStoryboard = UIStoryboard.home
-            guard let trashViewController = trashStoryboard.instantiateViewController(withIdentifier: TrashViewController.TRASH_VIEW_CONTROLLER) as? TrashViewController else {
-                fatalError("There is no identifier set as \(TrashViewController.TRASH_VIEW_CONTROLLER)")
-            }
-            return trashViewController
-        }
-    }
-    
-    static let SETTINGS_VIEW_CONTROLLER = "SettingsViewController"
-    static var settings: SettingsViewController {
-        get {
-            let settingsStoryboard = UIStoryboard.home
-            guard let settingsViewController = settingsStoryboard.instantiateViewController(withIdentifier: SettingsViewController.SETTINGS_VIEW_CONTROLLER) as? SettingsViewController else {
-                fatalError("There is no identifier set as \(SettingsViewController.SETTINGS_VIEW_CONTROLLER)")
-            }
-            return settingsViewController
         }
     }
 
--- NeverNote/ViewControllers/Home/HomeViewController.swift
+++ NeverNote/ViewControllers/Home/HomeViewController.swift
@@ -8,24 +8,40 @@
 
 import UIKit
 
-class HomeViewController: UITabBarController {
+class HomeViewController: UIViewController, UITabBarDelegate {
 
+    @IBAction func addNavigationButton(_ sender: Any?) {
+        let addScreenViewController = UIViewController.addScreen
+        self.present(addScreenViewController, animated: true, completion: nil)
+    }
+    @IBOutlet weak var goToNotesButton: UITabBarItem!
+    @IBOutlet weak var homeTabBar: UITabBar!
+    @IBOutlet weak var goToCompletedButton: UITabBarItem!
+    @IBOutlet weak var goToTrashButton: UITabBarItem!
+    @IBOutlet weak var goToSettingsButton: UITabBarItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        self.homeTabBar.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func addNavigationButton(_ sender: Any?) {
-        
-    }
-
-    
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        
-        guard let selectedPosition = self.tabBar.items?.index(of: item) else {
-            return
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if item.tag == 0 {
+            let notesViewController = UIViewController.notes
+            self.present(notesViewController, animated: true, completion: nil)
         }
-        
+        else if item.tag == 1 {
+            let completedTasksViewController = UIViewController.completed
+            self.present(completedTasksViewController, animated: true, completion: nil)
+        }
+        else if item.tag == 2 {
+            print("trash")
+        }
+        else {
+            print("settings")
+        }
     }
     
     override func didReceiveMemoryWarning() {
